ae89d8d8e2910b0dc1e6f7b88a396b00825ed1e7
Maciej Wiśniewski <m.wisniewski@elpromaelectronics.com>
2022-11-12 18:43:02 +0100

    Added quizzes model & migration

---
+++ database/migrations/2022_11_12_173011_create_quizzes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quizzes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('category');
+            $table->string('description');
+            $table->integer('number_of_questions');
+            $table->integer('questions');
+            $table->integer('answers');
+            $table->integer('successful');
+            $table->integer('fails');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quizzes');
+    }
+};
